6553a8d0e685633514d3712b947e7f73c837f7d6
klettermi <mi_aa_@naver.com>
2025-03-12 15:05:03 +0900

    feat: 게시판 CRUD Controller 작성

--- src/main/java/mi/board/domain/board/controller/BoardController.java
+++ src/main/java/mi/board/domain/board/controller/BoardController.java
@@ -1,8 +1,21 @@
 package mi.board.domain.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import mi.board.domain.board.dto.BoardRequestDto;
+import mi.board.domain.board.entity.Board;
 import mi.board.domain.board.service.BoardService;
-import org.springframework.web.bind.annotation.RestController;
+import mi.board.global.dto.ApiResponse;
+import mi.board.global.security.userdetails.UserDetailsImpl;
+import mi.board.global.utils.ResponseUtils;
+import org.apache.tomcat.util.http.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static mi.board.global.enums.SuccessCode.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,5 +23,36 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @PostMapping
+    public ApiResponse<?> createBoard(@RequestBody @Validated BoardRequestDto boardRequestDto,
+                             @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseUtils.success(BOARD_CREATE_SUCCESS, boardService.createBoard(boardRequestDto, userDetails));
+    }
 
+    @GetMapping
+    public ApiResponse<?> getBoardList() {
+        List<Board> boardList = boardService.getBoardList();
+        return ResponseUtils.success(BOARD_GET_SUCCESS, boardList);
+    }
+
+    @GetMapping("/{boardId}")
+    public ApiResponse<?> getBoardById(@PathVariable Long boardId) {
+        Board board = boardService.getBoardById(boardId);
+        return ResponseUtils.success(BOARD_GET_SUCCESS, board);
+    }
+
+    @PutMapping("/{boardId}")
+    public ApiResponse<?> updateBoard(@PathVariable Long boardId,
+                                      @RequestBody @Validated BoardRequestDto newBoardRequestDto,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Board updatedBoard = boardService.updateBoard(boardId, newBoardRequestDto, userDetails);
+        return ResponseUtils.success(BOARD_UPDATE_SUCCESS, updatedBoard);
+    }
+
+    @DeleteMapping("/{boardId}")
+    public ApiResponse<?> deleteBoard(@PathVariable Long boardId,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        boardService.deleteBoardById(boardId, userDetails);
+        return ResponseUtils.success(BOARD_DELETE_SUCCESS, null);
+    }
 }
--- src/main/java/mi/board/global/enums/SuccessCode.java
+++ src/main/java/mi/board/global/enums/SuccessCode.java
@@ -4,9 +4,15 @@ import lombok.Generated;
 import org.springframework.http.HttpStatus;
 
 public enum SuccessCode {
+    // USER
     USER_LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공했습니다."),
-    ARTICLE_CREATE_SUCCESS(HttpStatus.CREATED, "글 작성을 성공하였습니다.");
 
+    // BOARD
+    BOARD_CREATE_SUCCESS(HttpStatus.CREATED, "게시글 작성을 성공하였습니다."),
+    BOARD_GET_SUCCESS(HttpStatus.OK, "게시글 조회를 성공했습니다."),
+    BOARD_UPDATE_SUCCESS(HttpStatus.OK, "게시글 수정을 성공했습니다."),
+    BOARD_DELETE_SUCCESS(HttpStatus.OK, "게시글 삭제를 성공했습니다.")
+    ;
     private final HttpStatus httpStatus;
     private final String detail;
 
